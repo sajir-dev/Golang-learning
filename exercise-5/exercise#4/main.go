06dc1201ae180493d6fa7e7e9f4f94e587475292
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-08-31 09:01:11 +0530

    exercise-5

--- exercise-5/exercise#4/main.go
+++ exercise-5/exercise#4/main.go
@@ -3,13 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	student := struct {
-		name  string
-		grade string
+	// My solution
+	// student := struct {
+	// 	name  string
+	// 	grade string
+	// }{
+	// 	name:  "Raju",
+	// 	grade: "6",
+	// }
+
+	// fmt.Println(student.name, student.grade)
+
+	// Todd's solution
+	s := struct {
+		name      string
+		friends   map[string]int
+		favDrinks []string
 	}{
-		name:  "Raju",
-		grade: "6",
+		name: "James Bonda",
+		friends: map[string]int{
+			"moneypenny": 555,
+			"Q":          777,
+			"M":          888,
+		},
+		favDrinks: []string{
+			"martini",
+			"Water",
+		},
 	}
 
-	fmt.Println(student.name, student.grade)
+	fmt.Println(s.name)
+	fmt.Println(s.friends)
+	fmt.Println(s.favDrinks)
+
+	for k, v := range s.friends {
+		fmt.Println(k, v)
+	}
 }
